3d336fcb5541cfd65f296c76b587033906a40482
Jacob Barzee <jmbarzee@gmail.com>
2026-07-01 23:28:22 -0700

    twf: update formula for v0.10.1

--- Formula/twf.rb
+++ Formula/twf.rb
@@ -1,28 +1,28 @@
 class Twf < Formula
   desc "Toolchain for designing and validating entire Temporal systems in .twf"
   homepage "https://github.com/jmbarzee/temporal-architect"
-  version "0.10.0"
+  version "0.10.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/jmbarzee/temporal-architect/releases/download/v0.10.0/twf-v0.10.0-darwin-arm64.tar.gz"
-      sha256 "e116ca86f9389485ab67e0671f02a4472664ccbfc30ec50b1185b5369ea4ea43"
+      url "https://github.com/jmbarzee/temporal-architect/releases/download/v0.10.1/twf-v0.10.1-darwin-arm64.tar.gz"
+      sha256 "d622e93d478f495fc97dfc54dcf36d8e45a166a0e571238145dc7765ee02e0e6"
     end
     on_intel do
-      url "https://github.com/jmbarzee/temporal-architect/releases/download/v0.10.0/twf-v0.10.0-darwin-amd64.tar.gz"
-      sha256 "255981f8f2a5b3396c375e61c4d1f51623fc37a6170fc30c4e14da83566c2494"
+      url "https://github.com/jmbarzee/temporal-architect/releases/download/v0.10.1/twf-v0.10.1-darwin-amd64.tar.gz"
+      sha256 "1b78d0884ad3c2760ec77b0f0f8bc73887041887b6ae0cc102535fd3e35ba79f"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/jmbarzee/temporal-architect/releases/download/v0.10.0/twf-v0.10.0-linux-arm64.tar.gz"
-      sha256 "4d2a707418b49a76ffdc060ba1e6ece9f0223ddbadaacd73cf239d6e0e9d9db7"
+      url "https://github.com/jmbarzee/temporal-architect/releases/download/v0.10.1/twf-v0.10.1-linux-arm64.tar.gz"
+      sha256 "1015b2b85c76f0a8b63da9852ae01a92bb0b686d8fa6febdaf02693930fbcad4"
     end
     on_intel do
-      url "https://github.com/jmbarzee/temporal-architect/releases/download/v0.10.0/twf-v0.10.0-linux-amd64.tar.gz"
-      sha256 "5af49fdd1d2c88c1a3545d29424dc35312c8d431231bf578849a19abc5a40fcc"
+      url "https://github.com/jmbarzee/temporal-architect/releases/download/v0.10.1/twf-v0.10.1-linux-amd64.tar.gz"
+      sha256 "1e172e97272f33007167b8397ccf05eecf89ac94d912dafdcc1f2f535ed47bb5"
     end
   end
 
